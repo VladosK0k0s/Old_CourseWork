67e1186caeb6d7610c31a09f876a63015e7413af
Vladislav Stepaniuk <p.s.vlad2014@gmail.com>
2019-10-08 01:34:48 +0300

    Updates

---
+++ precache-manifest.c1def8889e50ac4f935cad490e50bacd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887e00f3f7f7b51f1548ad15f5b02e77",
+    "url": "/Old_CourseWork/index.html"
+  },
+  {
+    "revision": "b274f67b1db9811e5f6a",
+    "url": "/Old_CourseWork/static/css/main.89a22297.chunk.css"
+  },
+  {
+    "revision": "24ece0e51b081b153174",
+    "url": "/Old_CourseWork/static/js/2.11178c01.chunk.js"
+  },
+  {
+    "revision": "b274f67b1db9811e5f6a",
+    "url": "/Old_CourseWork/static/js/main.7e94fa1c.chunk.js"
+  },
+  {
+    "revision": "49ce1dd3cffaa973c5b0",
+    "url": "/Old_CourseWork/static/js/runtime-main.e6d8e2e4.js"
+  }
+]);
